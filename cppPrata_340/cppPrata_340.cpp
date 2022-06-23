28ae6b3a3c9bdbee6359d499d11d943a239bed7e
igor655 <belyyigor@gmail.com>
2022-06-23 07:47:10 +0300

    New task

--- cppPrata_340/cppPrata_340.cpp
+++ cppPrata_340/cppPrata_340.cpp
@@ -3,92 +3,25 @@
 
 #include <iostream>
 #include<Windows.h>
-#include<fstream>
-//#include<ios> //used to get stream size
-//#include<limits> //used to get numeric limits
+
 using namespace std;
+// struct 
 
-void q1(int n5, int j);
+struct polar
+{
+    double distance;
+    double angle;
+};
 
-void fun(int i);
-void fun2(int i);
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    // recursion
-    // Given a natural number n
-    // Print all numbers from 1 to n
-    // examples
-    // n = 5 --  1 2 3 4 5 
-    //n = -5 --  1 0 -1 -2 -3 -4 -5
-    // n = 0     1 0
-
    
-
-    //=============================
-    fun(5);
-    cout << endl;
-
-    fun2(-4);
-    cout << endl;
-    fun2(0);
-
-
-    
-
 return 0;
 }
 
-
-
-
-
-// recursion for positive numbers 
-void fun(int i)
-{
-    if (i > 0)
-    {
-        //cout << i-- << " ";
-        i--;
-        fun(i );
-    }
-    else return;
-    cout << i +1 << " ";
-
-   
-   
-
-}
-
-// recursion for negative numbers 
-void fun2(int i)
-{
-    
-    if (i  <= 1)
-    {
-    i++;
-    fun2(i);
-    }
-    else return;
-   
-    cout << i - 1 << " ";
-}
-
-
-
-
-//void fun2(int i)
-//{
-//    if (i <= 1)
-//    {
-//        i++;
-//    }
-//    else return;
-//    cout << i << " ";
-//    fun2(i);
-//}
 
 
