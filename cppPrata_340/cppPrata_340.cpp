a4e537337913b170367b557766384c39f0d5a7cf
igor bely <bellsax@rambler.ru>
2022-12-26 16:50:37 +0200

    reinstall vidusl studio

--- cppPrata_340/cppPrata_340.cpp
+++ cppPrata_340/cppPrata_340.cpp
@@ -14,62 +14,8 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    // recursion
-    // Given a natural number n
-    // Print all numbers from 1 to n
-
-    int n = 3;
-
-    /*if (n == 0)
-    {
-
-        cout << "1 " << "0 " << endl;
-    }
-    else if (n > 0)
-    {
-        for (size_t i = 1; i <= n; i++)
-        {
-            cout << i << " " ;
-
-        }
-        cout << endl;
-    }*/
-
-    q1(n +1 , 1);
-
-
-
 
 return 0;
-}
-
-
-void q1(int n5 , int j)
-{
-    
-   
-    if ( n5 > 0)
-    {
-        for (size_t i = 1; i < n5; i++)
-        {
-            cout << i << " ";
-
-        }
-    }
-    
-
-    if (j+1 < n5)
-    {
-        //cout << n5;
-        return;
-    }
-    else
-    {
-        q1(n5 , j - 1);
-        cout  << n5 - j << " ";
-    }
-
-    
 }
 
 
